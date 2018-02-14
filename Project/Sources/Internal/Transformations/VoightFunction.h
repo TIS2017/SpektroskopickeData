d20f9b538d20aa2aaa9d910a38d4ca979217b5e3
Patrik Fašang <patrik.fasang@gmail.com>
2018-02-14 16:56:04 +0100

    voigt #34

--- Project/Sources/Internal/Transformations/VoightFunction.h
+++ Project/Sources/Internal/Transformations/VoightFunction.h
@@ -11,16 +11,21 @@ namespace DataAnalysis {
 				mType = FT_VOIGHT;
 			}
 
-			void Initialize(__in const BaseType params) {
+			void Initialize(__in const double *params) {
 				mParams = params;
 				mInitialized = true;
 			}
-
 			virtual void Apply(__in const BaseType &in, __out BaseType &out) const {
+				double rx = (sqrt(log(2)) * (xi - (x0 + mParams[8])) / mParams[7]);
+				double ry = ((mParams[6] / mParams[7]) * sqrt(log(2)));
+				double vr, vi, vr0;
+				cwerf(0.0, ry, &vr0, &vi);
+				cwerf(rx, ry, &vr, &vi);
+				return a * vr / vr0;
 			}
 
 		protected:
-			BaseType mParams;
+			const double *mParams;
 		};
 	}
 }
