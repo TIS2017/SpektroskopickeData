d1e8b65733aab66b53c692c8aa34fd9b60686a55
Tomas Bordac <bordac6@uniba.sk>
2018-02-14 21:48:00 +0100

    Lorentz apply #27

--- Project/Sources/Internal/Transformations/PeakFunctions.h
+++ Project/Sources/Internal/Transformations/PeakFunctions.h
@@ -85,29 +85,7 @@ namespace DataAnalysis {
 			}
 
 			virtual void Apply(__in const BaseType &in, __out BaseType &out) const {
-				//out += Voigt(in);
-				double v = in;
-				static const std::complex<double> i(0.0, 1.0);
-				double w0 = mParams[6];
-				double td = w0 / 2;
-				double v0 = mParams[3];
-				double delta0 = mParams[8];
-
-
-				//counting z
-				std::complex<double> z = log(2) * (w0 + i*(v0 + delta0 - v)) / td;
-
-				//counting first multiplier
-				double first = 1 / abs(td);
-
-				//counting second multiplier
-				double second = sqrt(log(2) / M_PI);
-
-				//counting third multiplier
-				std::complex<double> third = pow(M_E, -pow(z, 2)) * erfc(-z.real());
-
-				//return first * second * third;
-				out += first * second * third.real();
+				out = in;
 			}
 
 		protected:
